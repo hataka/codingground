8b4ee834f00b599094304c13ec0c2fd3b0c2e7f6
kazuhiko hata <kazuhiko.hata@nifty.com>
2017-06-10 10:45:37 +0900

    modified by hataka 2017/06/10-08:38:56

--- android/YaSample/Sample011/app/src/main/java/ya/Sample011/Sample011.java
+++ android/YaSample/Sample011/app/src/main/java/ya/Sample011/Sample011.java
@@ -1,15 +1,16 @@
-// -*- mode: java -*-  Time-stamp: <2012-02-14 9:42:51 kahata>
+// -*- mode: java -*-  Time-stamp: <2017-06-10 10:40:07 kahata>
 /*================================================================
- * title: 
+ * title: はじめてのアンドロイドアプリケーション p.5
  * file: Sample011.java
- * path: F:\android\YASample\Sample011\src\ya\Sample011\Sample011.java
- * url:  http://192.168.24.92/android/YASample/Sample011/src/ya/Sample011/Sample011.java
+ * path: F:\GitHub\hataka\codingground\android\YaSample\Sample011\app\src\main\java\ya\Sample011\Sample011.java
+ * url:  http://192.168.1.53/f/GitHub/hataka/codingground/android/YaSample/Sample011/app/src/main/java/ya/Sample011/Sample011.java
  * created: Time-stamp: <2012-02-14 9:42:51 kahata>
+ * modified:Time-stamp: <2012-02-14 9:42:51 kahata>
  * revision: $Id$
  * Programmed By: kahata
  * To compile:
  * To run: 
- * link: 
+ * link: https://github.com/hataka/codingground/tree/master/android/YaSample/Sample011
  * description: はじめてのアンドロイドアプリケーション p.5
  *================================================================*/
 
@@ -55,9 +56,7 @@ public class Sample011 extends Activity {
 		// 選択されたIDを確認
 		switch (item.getItemId()) {
 			case 0:
-		    	// 指定したURLをWebviewに読み込む
-				//uri = Uri.parse("http://192.168.１.53/f/android/YASample/Sample011/src/ya/Sample011/Sample011.java");
-				//uri = Uri.parse("https://raw.githubusercontent.com/hataka/codingground/master/android/YaSample/Sample011/app/src/main/java/ya/Sample011/Sample011.java");
+	    	// 指定したURLをWebviewに読み込む
 				uri = Uri.parse(RAW_SITE + className+"/app/src/main/java/ya/"+ className+"/"+ className+".java");
 			i = new Intent(Intent.ACTION_VIEW,uri);
 			startActivity(i);
@@ -66,8 +65,6 @@ public class Sample011 extends Activity {
 				break;
 			case 1:
 		    // 指定したURLをWebviewに読み込む
-				//uri = Uri.parse("http://192.168.1.53/f/android/YASample/Sample011/res/layout/main.xml");
-				//uri = Uri.parse("https://raw.githubusercontent.com/hataka/codingground/master/android/YaSample/Sample011/app/src/main/res/layout/main.xml");
 				uri = Uri.parse(GIT_SITE+ className);
 				i = new Intent(Intent.ACTION_VIEW,uri);
 				startActivity(i);
