276d7b5bf6a686acfe7cbe448465d33b1e97ca62
kazuhiko hata <kazuhiko.hata@nifty.com>
2017-06-21 09:56:44 +0900

    modified by hataka 2017/06/21-09:55:06

---
+++ android/Gudon/ToOtherActivity1/app/src/main/java/gudon/sample/ToOtherActivity1/MainActivity.java
@@ -0,0 +1,32 @@
+package gudon.sample.ToOtherActivity1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        Button button = new Button(this);
+        button.setText("トーストを表示");
+        setContentView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        		Context context = getApplicationContext();
+        		CharSequence text = "Hello toast!";
+        		int duration = Toast.LENGTH_SHORT;
+
+        		Toast toast = Toast.makeText(context, text, duration);
+        		toast.show();
+            }
+        });
+
+	}
+}
